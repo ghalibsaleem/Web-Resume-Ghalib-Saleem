4292bb1acbc197c40b48d188f453a6fcfbe92164
Ghalib Saleem <ghalib.saleem.123@gmail.com>
2020-10-28 02:53:33 -0400

    Add project files.

---
+++ Resume/Startup.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Resume
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSameOrigin",
+                    policy => policy
+                    .WithOrigins("https://www.ghalibsaleem.me")
+                    //.AllowAnyOrigin()
+                    );
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseHttpsRedirection();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseCors("AllowSameOrigin");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
+                app.UseRewriter(
+                    new RewriteOptions()
+                        .AddRedirectToWww()
+                        .AddRedirectToHttps()
+                );
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
